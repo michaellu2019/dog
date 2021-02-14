091852af46685321f8fbb96f70b0eb1feb4047ef
michaellu2019 <mlu0708@mit.edu>
2021-02-13 21:01:52 -0500

    Servo configuration

--- constants.py
+++ constants.py
@@ -44,6 +44,6 @@ gait_divs = STEP_SIZE/STEP_SPEED
 servo_channels = [[0, 1, 2], [4, 5, 6], [8, 9, 10], [12, 13, 14]] # leg component order: [foot, knee, shoulder]
 servo_neutral_vals = [[90, 90, 90], [90, 90, 90], [90, 90, 90], [90, 90, 90]]
 # servo_neutral_vals = [[90, 45, 90], [90, 45, 90], [90, 45, 90], [90, 45, 90]]
-servo_offsets = [[0, 0, 0], [0, 0, -1], [0, 0, -2], [0, 0, -5]]
+servo_offsets = [[0, 0, 0], [0, 0, -1], [0, 0, -5], [0, 0, -5]]
 #servo_offsets = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 servo_vals = [[servo_neutral_vals[i][j] for j in range(NUM_LEG_SERVOS)] for i in range(NUM_LEGS)]
